7850060fb202380fc151139b2b270e8bdbd3dbf9
Ignacio Arriagada <iarriagada@zionit.cl>
2021-10-12 18:13:14 -0300

    cambiando spinner

--- src/components/ui/loading/Loading.jsx
+++ src/components/ui/loading/Loading.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Loader from "react-loader-spinner";
 
 function Loading({ isLoading = false }) {
   return (
@@ -7,8 +6,8 @@ function Loading({ isLoading = false }) {
       {
         isLoading && (
           <div className="fixed flex items-center justify-center w-full min-h-screen bg-black bg-opacity-40 z-loading">
-            <div className="p-5 bg-white rounded-full">
-              <Loader type="TailSpin" color="#1F2937" />
+            <div className="h-20 w-20 bg-white rounded-full flex items-center justify-center">
+              <i className="fas fa-spinner animate-spin fa-3x text-gray-500"></i>
             </div>
           </div>)
       }
